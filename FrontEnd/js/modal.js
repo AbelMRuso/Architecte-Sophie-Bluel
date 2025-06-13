af68d4b330969ff0e8b506115667393ee6fde1c3
MrAbelDev <abelmruso@gmail.com>
2025-06-13 13:48:33 +0200

    restructuring modal.js file

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,38 +1,29 @@
-//function to display images
-
-async function modalWorks() {
-    //array con todos los trabajos
-    let allWorks = await getWorks();
-    const modalContent = document.getElementById("modal-images");
-    modalContent.innerHTML = "";
-
-    for (let i = 0; i < allWorks.length; i++) {
-        const imgModal = allWorks[i].imageUrl;
-        const worksContent = document.createElement("figure");
-        const imgContent = document.createElement("img");
-        const deleteImg = document.createElement("button");
-        deleteImg.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-
-        imgContent.src = imgModal;
-        worksContent.appendChild(imgContent);
-        modalContent.appendChild(worksContent);
-        worksContent.appendChild(deleteImg);
-    }
-}
-
-//retrieving DOM elements to work on events
+//DOM elements
 const modalDiv = document.getElementById("modal");
 const closeModal = document.getElementById("close-modal");
 const backButton = document.getElementById("back-button");
 const modalButtons = document.querySelector(".modal-buttons");
 const buttonModal = document.getElementById("button-modal");
 const titleModal = document.querySelector(".title-modal h2");
-const formPhoto = document.getElementById("modal-form");
+const modalForm = document.getElementById("modal-form");
 const modalImages = document.getElementById("modal-images");
 const overlay = document.getElementById("overlay");
 const inputTitle = document.getElementById("title");
-//creamos variable para aplicar un mensaje de error en la validación del formulario
 const messageError = document.querySelector(".error-message");
+const categoryFormModal = document.getElementById("category");
+const inputFile = document.getElementById("div-photo");
+const preview = document.getElementById("preview");
+const iconImage = document.getElementById("icon-image");
+const addPhotoBtn = document.getElementById("add-photo-btn");
+const textAddImg = document.getElementById("text-add-img");
+const formModal = document.getElementById("formModal");
+const formTitle = document.getElementById("title");
+
+//Modal views
+let currentView = "gallery";
+
+//variable para evitar que las categorias se recargen en la modal si el usuario clica varias veces sobre la flecha
+let categoriesLoaded = false;
 
 //modal is hidden by default
 overlay.classList.add("hidden");
@@ -49,60 +40,21 @@ closeModal.addEventListener("click", () => {
     resetForm();
 });
 
+//close modal when click outside
 overlay.addEventListener("click", (event) => {
     if (event.target === overlay) {
         overlay.classList.add("hidden");
     }
 });
 
-//Modal views
-let currentView = "gallery";
-
 //go to "ajout photo" inside modal by clicking the button "ajouter une photo"
 buttonModal.addEventListener("click", () => {
     if (currentView === "gallery") {
         displayFormModal();
     } else {
-        // AÑADIREMOS ESTA VALIDACIÓN MÁS ADELANTE
+        // ¿NO DEBERÍAMOS AÑADIR AQUÍ LA VALIDACIÓN DEL FORMULARIO?
     }
 });
-
-//function that handles events when the gallery is displayed in the modal.
-async function displayGalleryModal() {
-    await modalWorks();
-    overlay.classList.remove("hidden");
-    modalButtons.classList.add("close-button");
-    formPhoto.classList.add("hidden");
-    modalImages.classList.remove("hidden");
-    backButton.classList.add("hidden");
-    titleModal.innerText = "Galerie photo";
-    buttonModal.classList.remove("valider");
-    buttonModal.classList.add("ajouter");
-    buttonModal.setAttribute("type", "button");
-    buttonModal.removeAttribute("form");
-    buttonModal.innerText = "Ajouter une photo";
-
-    currentView = "gallery";
-}
-
-//function that handles events when the form is displayed in the modal.
-async function displayFormModal() {
-    modalImages.classList.add("hidden");
-    formPhoto.classList.remove("hidden");
-    backButton.classList.remove("hidden");
-    modalButtons.classList.remove("close-button");
-    modalButtons.classList.add("separate-buttons");
-    titleModal.innerText = "Ajout photo";
-    buttonModal.classList.remove("ajouter");
-    buttonModal.classList.add("valider");
-    buttonModal.setAttribute("type", "submit");
-    buttonModal.setAttribute("form", "formModal");
-    buttonModal.innerText = "Valider";
-    messageError.classList.add("hidden");
-    messageError.innerText = "";
-
-    currentView = "form";
-}
 
 // go back with back-button
 backButton.addEventListener("click", () => {
@@ -111,12 +63,6 @@ backButton.addEventListener("click", () => {
 });
 
 /* INTEGRACIÓN CATEGORIAS API EN FORMULARIO DE LA MODAL */
-
-const modalForm = document.getElementById("modal-form");
-const titleFormModal = document.getElementById("title");
-const categoryFormModal = document.getElementById("category");
-let categoriesLoaded = false;
-
 categoryFormModal.addEventListener("click", () => {
     if (!categoriesLoaded) {
         categoriesList();
@@ -124,22 +70,7 @@ categoryFormModal.addEventListener("click", () => {
     }
 });
 
-async function categoriesList() {
-    const categoriesForm = await getCategories();
-    for (let i = 0; i < categoriesForm.length; i++) {
-        const categoriesOption = categoriesForm[i].name;
-        const categoriesId = categoriesForm[i].id;
-
-        const categoryContent = document.createElement("option");
-        categoryContent.setAttribute("value", categoriesId);
-        categoryContent.textContent = categoriesOption;
-
-        categoryFormModal.appendChild(categoryContent);
-    }
-}
-
 //Función que conecta con la api
-
 async function initModalForm(formOptionsModal) {
     const token = localStorage.getItem("token");
 
@@ -154,20 +85,10 @@ async function initModalForm(formOptionsModal) {
         displayWorks();
         formModal.reset();
         resetForm();
-        messageError.classList.remove("message-error");
-        messageError.classList.add("confirmation-message");
-        messageError.classList.remove("hidden");
-        messageError.innerText = "Votre projet est enregistré correctement";
     }
 }
 
 //MOSTRAR LA IMAGEN SELECCIONADA EN EL INPUT
-const inputFile = document.getElementById("div-photo");
-const showImg = document.getElementById("select-file");
-const preview = document.getElementById("preview");
-const iconImage = document.getElementById("icon-image");
-const addPhotoBtn = document.getElementById("add-photo-btn");
-const textAddImg = document.getElementById("text-add-img");
 
 inputFile.addEventListener("change", () => {
     const file = inputFile.files[0];
@@ -188,8 +109,6 @@ inputFile.addEventListener("change", () => {
     reader.readAsDataURL(file); // Leemos el archivo como URL de imagen
 });
 
-const formModal = document.getElementById("formModal");
-
 //COMPORTAMIENTO DE LA PÁGINA AL HACER SUBMIT EN EL FORMULARIO
 formModal.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -200,11 +119,9 @@ formModal.addEventListener("submit", (event) => {
 
     if (!formImg || formTitleValue.trim() === "" || !categoryValue) {
         messageError.classList.remove("hidden");
-        messageError.innerText = "veuillez remplir tous les champs du formulaire";
+        messageError.innerText = "Veuillez remplir tous les champs du formulaire";
         return;
     }
-
-    messageError.classList.add("hidden");
 
     const formData = new FormData();
 
@@ -212,6 +129,7 @@ formModal.addEventListener("submit", (event) => {
     formData.append("title", formTitleValue);
     formData.append("category", categoryValue);
 
+    //Objeto de configuración
     const formOptionsModal = {
         method: "post",
         body: formData,
@@ -220,7 +138,6 @@ formModal.addEventListener("submit", (event) => {
 });
 
 //función para reiniciar el formulario
-
 function resetForm() {
     preview.src = "";
     inputTitle.value = "";
@@ -229,13 +146,9 @@ function resetForm() {
     iconImage.classList.remove("hidden");
     addPhotoBtn.classList.remove("hidden");
     textAddImg.classList.remove("hidden");
-    messageError.classList.add("hidden");
-    messageError.innerText = "";
 }
 
-//PELEA MAXIMA PARA QUE EL BOTON DEL FORMULARIO PUEDA VALIDARLO Y CAMBIE DE COLOR
-const formTitle = document.getElementById("title");
-
+//hacer que el boton del formulario haga submit y cambie de color cuando los 3 campos se han completado
 formTitle.addEventListener("input", checkForm);
 categoryFormModal.addEventListener("change", checkForm);
 inputFile.addEventListener("change", checkForm);
@@ -249,5 +162,78 @@ function checkForm() {
         buttonModal.classList.remove("valider-green");
         buttonModal.classList.add("valider");
         buttonModal.disabled = true;
+    }
+}
+
+//********** FUNCTIONS ********/
+
+//Función que recoge los datos de Works de la API, recorre su array y crea un elemento para cada uno de los objetos
+async function modalWorks() {
+    //array con todos los trabajos
+    let allWorks = await getWorks();
+    const modalContent = document.getElementById("modal-images");
+    modalContent.innerHTML = "";
+
+    for (let i = 0; i < allWorks.length; i++) {
+        const imgModal = allWorks[i].imageUrl;
+        const worksContent = document.createElement("figure");
+        const imgContent = document.createElement("img");
+        const deleteImg = document.createElement("button");
+        deleteImg.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+
+        imgContent.src = imgModal;
+        worksContent.appendChild(imgContent);
+        modalContent.appendChild(worksContent);
+        worksContent.appendChild(deleteImg);
+    }
+}
+
+//function that handles events when the gallery is displayed in the modal.
+async function displayGalleryModal() {
+    await modalWorks();
+    overlay.classList.remove("hidden");
+    modalButtons.classList.add("close-button");
+    modalForm.classList.add("hidden");
+    modalImages.classList.remove("hidden");
+    backButton.classList.add("hidden");
+    titleModal.innerText = "Galerie photo";
+    buttonModal.classList.remove("valider");
+    buttonModal.classList.add("ajouter");
+    buttonModal.setAttribute("type", "button");
+    buttonModal.removeAttribute("form");
+    buttonModal.innerText = "Ajouter une photo";
+
+    currentView = "gallery";
+}
+
+//function that handles events when the form is displayed in the modal.
+async function displayFormModal() {
+    modalImages.classList.add("hidden");
+    modalForm.classList.remove("hidden");
+    backButton.classList.remove("hidden");
+    modalButtons.classList.remove("close-button");
+    modalButtons.classList.add("separate-buttons");
+    titleModal.innerText = "Ajout photo";
+    buttonModal.classList.remove("ajouter");
+    buttonModal.classList.add("valider");
+    buttonModal.setAttribute("type", "submit");
+    buttonModal.setAttribute("form", "formModal");
+    buttonModal.innerText = "Valider";
+
+    currentView = "form";
+}
+
+//Función que recoge las categorías de la API, y las inserta en el section de la modal en forma de option
+async function categoriesList() {
+    const categoriesForm = await getCategories();
+    for (let i = 0; i < categoriesForm.length; i++) {
+        const categoriesOption = categoriesForm[i].name;
+        const categoriesId = categoriesForm[i].id;
+
+        const categoryContent = document.createElement("option");
+        categoryContent.setAttribute("value", categoriesId);
+        categoryContent.textContent = categoriesOption;
+
+        categoryFormModal.appendChild(categoryContent);
     }
 }
